34e8ddebfc8bdecf22610526229bac69fa93b172
Muhammad Saad <muhammadsaad3119@gmail.com>
2021-09-29 23:22:10 +0500

    Add project files.

---
+++ MultipleFiles-Upload-Download-aspnetcoreapi/Controllers/FileController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MultipleFiles_Upload_Download_aspnetcoreapi.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MultipleFiles_Upload_Download_aspnetcoreapi.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FileController : ControllerBase
+    {
+        #region Property  
+        private readonly IFileService _fileService;
+        #endregion
+
+        #region Constructor  
+        public FileController(IFileService fileService)
+        {
+            _fileService = fileService;
+        }
+       
+        #endregion
+
+        #region Upload  
+        [HttpPost(nameof(Upload))]
+        public IActionResult Upload([Required] List<IFormFile> formFiles, [Required] string subDirectory)
+        {
+            try
+            {
+                _fileService.UploadFile(formFiles, subDirectory);
+
+                return Ok(new { formFiles.Count, Size = _fileService.SizeConverter(formFiles.Sum(f => f.Length)) });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        #endregion
+
+        #region Download File  
+        [HttpGet(nameof(Download))]
+        public IActionResult Download([Required] string subDirectory)
+        {
+
+            try
+            {
+                var (fileType, archiveData, archiveName) = _fileService.DownloadFiles(subDirectory);
+
+                return File(archiveData, fileType, archiveName);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+        }
+        #endregion
+    }
+}
